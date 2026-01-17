9f47d5a385a5c4a774c34be9a666fea739b9629c
Dave Smith <davesmith.00000@gmail.com>
2026-01-17 15:20:09 +0000

    Support building on Windows

--- build.ps1
+++ build.ps1
@@ -1,15 +1,19 @@
-function build {
-    param (
-        [string]$ProjectPath
-    )
+# PowerShell build script for Windows
 
-    echo ">>> $ProjectPath"
-    Set-Location -Path $ProjectPath -PassThru
-    & .\build.ps1
-    Set-Location -Path .. -PassThru
-    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-}
+$ErrorActionPreference = "Stop"
 
-build "indigo-plugin"
-build "sbt-indigo"
-build "indigo"
+.\mill.bat --no-server __.compile
+.\mill.bat --no-server __.reformat
+.\mill.bat --no-server -j2 __.fix
+.\mill.bat --no-server -j2 __.fastLinkJS
+.\mill.bat --no-server -j2 __.fastLinkJSTest
+.\mill.bat --no-server __.test
+.\mill.bat --no-server __.publishLocal
+
+# Will return when sbt 2.0 supports Scala.js
+
+# SBT Indigo
+# Write-Host ">>> SBT-Indigo"
+# Set-Location sbt-indigo
+# .\build.ps1
+# Set-Location ..
