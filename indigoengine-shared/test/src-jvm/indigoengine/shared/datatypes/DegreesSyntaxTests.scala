76cbc1833fd258a3a2ea434965317c64ad02f8ba
davesmith00000 <davesmith.00000@gmail.com>
2026-02-24 21:31:20 +0000

    Fixed Degrees and Radians to retain existing functionality on JS and Native

---
+++ indigoengine-shared/test/src-jvm/indigoengine/shared/datatypes/DegreesSyntaxTests.scala
@@ -0,0 +1,12 @@
+package indigoengine.shared.datatypes
+
+class DegreesSyntaxTests extends munit.FunSuite {
+  
+  test("Degrees can do simple math with Doubles") {
+    assertEquals(Degrees(10) + 2, Degrees(12))
+    assertEquals(Degrees(10) - 2, Degrees(8))
+    assertEquals(Degrees(10) * 2, Degrees(20))
+    assertEquals(Degrees(10) / 2, Degrees(5))
+  }
+
+}
---
+++ indigoengine-shared/test/src-jvm/indigoengine/shared/datatypes/RadiansSyntaxTests.scala
@@ -0,0 +1,12 @@
+package indigoengine.shared.datatypes
+
+class RadiansSyntaxTests extends munit.FunSuite {
+  
+  test("Radians can do simple math with Doubles") {
+    assertEquals(Radians(10) + 2, Radians(12))
+    assertEquals(Radians(10) - 2, Radians(8))
+    assertEquals(Radians(10) * 2, Radians(20))
+    assertEquals(Radians(10) / 2, Radians(5))
+  }
+
+}
