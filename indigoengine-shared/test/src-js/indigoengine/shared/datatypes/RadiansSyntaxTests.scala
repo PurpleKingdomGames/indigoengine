eae220aa16881ca71d3d6f0fd6213875693a1d91
davesmith00000 <davesmith.00000@gmail.com>
2026-03-01 21:13:27 +0000

    More formatting...

--- indigoengine-shared/test/src-js/indigoengine/shared/datatypes/RadiansSyntaxTests.scala
+++ indigoengine-shared/test/src-js/indigoengine/shared/datatypes/RadiansSyntaxTests.scala
@@ -1,7 +1,7 @@
 package indigoengine.shared.datatypes
 
 class RadiansSyntaxTests extends munit.FunSuite {
-  
+
   test("Radians can do simple math with Doubles") {
     assertEquals(Radians(10) + 2, Radians(12))
     assertEquals(Radians(10) - 2, Radians(8))
