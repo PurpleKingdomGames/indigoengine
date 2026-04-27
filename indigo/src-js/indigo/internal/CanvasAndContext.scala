942a435fc7742d38c0565f44a410b486fd898d32
davesmith00000 <davesmith.00000@gmail.com>
2026-04-27 07:55:04 +0100

    Tyrian makes the canvas

--- indigo/src-js/indigo/internal/CanvasAndContext.scala
+++ indigo/src-js/indigo/internal/CanvasAndContext.scala
@@ -3,63 +3,12 @@ package indigo.internal
 import indigo.render.facades.WebGL2RenderingContext
 import indigo.core.utils.IndigoLogger
 import org.scalajs.dom
-import org.scalajs.dom.Element
 import org.scalajs.dom.html
 
 import scala.annotation.nowarn
 import scala.scalajs.js.Dynamic
 
 object CanvasAndContext:
-
-  @SuppressWarnings(
-    Array(
-      "scalafix:DisableSyntax.null",
-      "scalafix:DisableSyntax.var"
-    )
-  )
-  @nowarn("msg=unused")
-  def setupCanvas(width: Int, height: Int, parentElement: Element): html.Canvas =
-    val defaultName = "indigo-container"
-
-    val parentElementId =
-      Option(parentElement.id)
-        .map(id => if id.isEmpty then defaultName else id)
-        .getOrElse(defaultName)
-
-    val name = s"$parentElementId-canvas"
-
-    var canvas: html.Canvas =
-      dom.document.getElementById(name).asInstanceOf[html.Canvas]
-
-    if canvas == null then
-      canvas = dom.document.createElement("canvas").asInstanceOf[html.Canvas]
-
-      parentElement.appendChild(canvas)
-
-      canvas.id = name
-      canvas.width = width
-      canvas.height = height
-
-    canvas
-
-  @SuppressWarnings(
-    Array(
-      "scalafix:DisableSyntax.null",
-      "scalafix:DisableSyntax.var"
-    )
-  )
-  @nowarn("msg=unused")
-  def createNamedCanvas(width: Int, height: Int, name: String): html.Canvas =
-    var canvas: html.Canvas =
-      dom.document.getElementById(name).asInstanceOf[html.Canvas]
-
-    if canvas == null then
-      canvas = dom.document.createElement("canvas").asInstanceOf[html.Canvas]
-      canvas.id = name
-      canvas.width = width
-      canvas.height = height
-
-    canvas
 
   def setupContext(canvas: html.Canvas): WebGL2RenderingContext =
     // TODO: These should come from somewhere.
@@ -77,6 +26,20 @@ object CanvasAndContext:
     ensureRenderingTechnologyAvailable(args)
 
     canvas.getContext("webgl2", args).asInstanceOf[WebGL2RenderingContext]
+
+  @SuppressWarnings(Array("scalafix:DisableSyntax.null", "scalafix:DisableSyntax.var"))
+  @nowarn("msg=unused")
+  private def createNamedCanvas(width: Int, height: Int, name: String): html.Canvas =
+    var canvas: html.Canvas =
+      dom.document.getElementById(name).asInstanceOf[html.Canvas]
+
+    if canvas == null then
+      canvas = dom.document.createElement("canvas").asInstanceOf[html.Canvas]
+      canvas.id = name
+      canvas.width = width
+      canvas.height = height
+
+    canvas
 
   @SuppressWarnings(
     Array(
