e324fd409f1996406e84bc55b7336b2b3e714c60
davesmith00000 <davesmith.00000@gmail.com>
2026-05-09 10:56:15 +0100

    Added a test.sh script

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+./mill --no-server __.compile
+./mill --no-server __.reformat
+./mill --no-server __.compile
+./mill --no-server -j2 __.fix
+./mill --no-server -j2 __.fastLinkJSTest
+./mill --no-server -j2 __.test.nativeLink
+./mill --no-server -j2 __.test
