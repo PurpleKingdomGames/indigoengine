1616a941ab2821257efed464aaaba9360ae75d5b
davesmith00000 <davesmith.00000@gmail.com>
2026-05-10 09:16:56 +0100

    Reduced sandbox native to the minimum

--- indigo-sandboxes/sandbox/src-native/main/scala/com/example/sandbox/scenes/ScenesList.scala
+++ indigo-sandboxes/sandbox/src-native/main/scala/com/example/sandbox/scenes/ScenesList.scala
@@ -7,40 +7,4 @@ import indigo.*
 object ScenesList:
 
   val scenes: NonEmptyBatch[Scene[SandboxStartupData, SandboxGameModel]] =
-    NonEmptyBatch(
-      Scene.empty
-        // OriginalScene,
-        // ShapesScene,
-        // LightsScene,
-        // RefractionScene,
-        // LegacyEffectsScene,
-        // BoundsScene,
-        // CameraScene,
-        // TextureTileScene,
-        // ConfettiScene,
-        // MutantsScene,
-        // CratesScene,
-        // ClipScene,
-        // TextScene,
-        // BoxesScene,
-        // ManyEventHandlers,
-        // TimelineScene,
-        // UltravioletScene,
-        // BoundingCircleScene,
-        // LineReflectionScene,
-        // CameraWithCloneTilesScene,
-        // PathFindingScene,
-        // CaptureScreenScene,
-        // NineSliceScene,
-        // SfxScene,
-        // ComponentUIScene,
-        // ComponentUIScene2,
-        // WindowsScene,
-        // MeshScene,
-        // WaypointScene,
-        // ActorPoolScene,
-        // ActorPoolPhysicsScene,
-        // PerformerScene,
-        // PerformerPhysicsScene,
-        // ViewportResizeScene
-    )
+    NonEmptyBatch(Scene.empty)
