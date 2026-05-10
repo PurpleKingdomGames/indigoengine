a72b8514cbe2dc42f81b1b4c68fe69f366d450a5
davesmith00000 <davesmith.00000@gmail.com>
2026-05-10 09:16:56 +0100

    Reduced sandbox native to the minimum

--- indigo-sandboxes/sandbox/src-native/main/scala/com/example/sandbox/SandboxGame.scala
+++ indigo-sandboxes/sandbox/src-native/main/scala/com/example/sandbox/SandboxGame.scala
@@ -1,19 +1,13 @@
 package com.example.sandbox
 
 import com.example.sandbox.scenes.*
-// import example.TestFont
 import indigo.*
-// import indigo.json.Json
-// import indigoextras.effectmaterials.LegacyEffects
-// import indigoextras.effectmaterials.Refraction
-// import indigoextras.subsystems.FPSCounter
 
 final class SandboxGame extends Game[SandboxBootData, SandboxStartupData, SandboxGameModel]:
 
   val gameId: GameId = GameId("sandbox-native")
 
   def initialScene(bootData: SandboxBootData): Option[SceneName] =
-    // Some(WindowsScene.name)
     None
 
   def scenes(bootData: SandboxBootData): NonEmptyBatch[Scene[SandboxStartupData, SandboxGameModel]] =
@@ -24,38 +18,9 @@ final class SandboxGame extends Game[SandboxBootData, SandboxStartupData, Sandbo
   def boot(args: Array[String]): Outcome[BootResult[SandboxBootData, SandboxGameModel]] =
     Outcome(
       BootResult(
-        EngineConfig.default
-          .withClearColor(RGBA(0.4, 0.2, 0.5, 1)),
-        SandboxBootData() // args.find(_ == "key").getOrElse("No entry for 'key'."))
+        EngineConfig.default,
+        SandboxBootData()
       )
-      // .withAssets(
-      //   // TODO: For goodness sake, use the asset listing generator.
-      //   SandboxAssets.assets((os.pwd / "indigo-sandboxes" / "sandbox").toString + "/") ++
-      //     Shaders.assets((os.pwd / "indigo-sandboxes" / "sandbox").toString + "/") ++
-      //     Archetype.assets((os.pwd / "indigo-sandboxes" / "sandbox").toString + "/")
-      // ).withFonts(
-      //   Fonts.fontInfo,
-      //   TestFont.fontInfo
-      // ).withSubSystems(
-      //   FPSCounter[SandboxGameModel](
-      //     Fonts.fontKey,
-      //     SandboxAssets.smallFontName
-      //   )
-      //     .withLayerKey(Constants.LayerKeys.fps)
-      //     .placeAt { (context, bounds) =>
-      //       Point(0, context.frame.viewport.height - bounds.height)
-      //     }
-      // ).withShaders(
-      //   Shaders.circle,
-      //   Shaders.external,
-      //   Shaders.sea,
-      //   LegacyEffects.entityShader,
-      //   Archetype.shader,
-      //   UVShaders.circle,
-      //   UVShaders.voronoi,
-      //   UVShaders.redBlend
-      // ).addShaders(Refraction.shaders)
-      //   .addShaders(indigoextras.ui.shaders.all)
     )
 
   def setup(
@@ -63,46 +28,15 @@ final class SandboxGame extends Game[SandboxBootData, SandboxStartupData, Sandbo
       assetCollection: AssetCollection,
       dice: Dice
   ): Outcome[Startup[SandboxStartupData]] =
-    // println(bootData.message)
-
-    // def makeStartupData(
-    //     aseprite: Aseprite,
-    //     spriteAndAnimations: SpriteAndAnimations,
-    //     clips: Map[CycleLabel, Clip[Material.Bitmap]]
-    // ): Startup.Success[SandboxStartupData] =
-    //   Startup
-    //     .Success(
-    //       SandboxStartupData(
-    //         // Dude(
-    //         //   aseprite,
-    //         //   spriteAndAnimations.sprite
-    //         //     .withRef(16, 16) // Initial offset, so when talk about his position it's the center of the sprite
-    //         //     .moveTo(SandboxGame.screenCenter) // Also place him in the middle of the screen initially
-    //         //     .withMaterial(SandboxAssets.dudeMaterial),
-    //         //   clips
-    //         // )
-    //       )
-    //     )
-    //     .addAnimations(spriteAndAnimations.animations)
-
-    // val res: Option[Startup.Success[SandboxStartupData]] = for {
-    //   json                <- assetCollection.findTextDataByName(AssetName(SandboxAssets.dudeName.toString + "-json"))
-    //   aseprite            <- Json.asepriteFromJson(json)
-    //   spriteAndAnimations <- aseprite.toSpriteAndAnimations(dice, SandboxAssets.dudeName)
-    //   clips               <- aseprite.toClips(SandboxAssets.dudeName)
-    // } yield makeStartupData(aseprite, spriteAndAnimations, clips)
-
-    // Outcome(res.getOrElse(Startup.Failure("Failed to load the dude")))
     Outcome(Startup.Success(SandboxStartupData()))
 
   def initialModel(startupData: SandboxStartupData): Outcome[SandboxGameModel] =
-    Outcome(SandboxModel.initialModel(startupData))
+    Outcome(SandboxModel.initialModel)
 
   def updateModel(
       context: Context[SandboxStartupData],
       model: SandboxGameModel
   ): GlobalEvent => Outcome[SandboxGameModel] =
-    // SandboxModelShared.updateModel(context, model)
     case _ => Outcome(model)
 
   def present(
@@ -110,37 +44,8 @@ final class SandboxGame extends Game[SandboxBootData, SandboxStartupData, Sandbo
       model: SandboxGameModel
   ): Outcome[SceneUpdateFragment] =
     Outcome(SceneUpdateFragment.empty)
-    // Outcome(
-    //   SceneUpdateFragment(
-    //     Constants.LayerKeys.background -> Layer.Stack.empty,
-    //     Constants.LayerKeys.game       -> Layer.Stack.empty,
-    //     Constants.LayerKeys.windows    -> Layer.Stack.empty,
-    //     Constants.LayerKeys.fps ->
-    //       Layer.empty
-    //         .withCamera(Camera.default)
-    //         .withMagnification(1)
-    //   )
-    // )
 
-object SandboxGame:
-
-  val gameWidth: Int      = 228
-  val gameHeight: Int     = 128
-  val viewportWidth: Int  = gameWidth * 2  // 456
-  val viewportHeight: Int = gameHeight * 2 // 256
-  val screenCenter: Point =
-    Point(viewportWidth, viewportHeight) / 2
-
-// final case class Dude(
-//     aseprite: Aseprite,
-//     sprite: Sprite[Material.ImageEffects],
-//     clips: Map[CycleLabel, Clip[Material.Bitmap]]
-// )
-final case class SandboxBootData( /*message: String*/ )
-final case class SandboxStartupData( /*dude: Dude*/ )
-// final case class SandboxViewModel(
-//     // offset: Point,
-//     // useLightingLayer: Boolean
-// )
+final case class SandboxBootData()
+final case class SandboxStartupData()
 
 final case class Log(message: String) extends GlobalEvent
