10ec8410d7e0ebf97564f9b4cca45df3e98bdc7a
davesmith00000 <davesmith.00000@gmail.com>
2026-05-24 14:43:05 +0100

    FIXED #99: TerminalText shader / UV bug

--- roguelike-starterkit/test/src/roguelikestarterkit/terminal/TerminalTextTests.scala
+++ roguelike-starterkit/test/src/roguelikestarterkit/terminal/TerminalTextTests.scala
@@ -1,14 +1,25 @@
 package roguelikestarterkit.terminal
 
+// import ultraviolet.utils.ShaderASTPrinter
+
 class TerminalTextTests extends munit.FunSuite {
 
   test("Validate the terminal text shader") {
     import ultraviolet.syntax.*
 
-    val actual =
-      TerminalText.ShaderImpl.frag.toGLSL300.code
+    inline def fragment =
+      TerminalText.ShaderImpl.frag
 
+    val actual =
+      fragment.toGLSL300.code
+    
+    // val outputAST = ShaderASTPrinter.printAST(fragment)
+    // val outputASTTransformed = ShaderASTPrinter.printASTTransformed(fragment, ultraviolet.datatypes.ProgramVersion.GLSL_300)
     // println(actual)
+    // println("---")
+    // println(outputAST)
+    // println("---")
+    // println(outputASTTransformed)
 
     assert(actual.nonEmpty)
   }
