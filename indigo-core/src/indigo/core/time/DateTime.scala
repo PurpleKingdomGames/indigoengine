97874f295a09a0b5d24ccd1e0c218061eb77ca0d
David North <hobnob@users.noreply.github.com>
2026-07-28 11:46:39 +0100

    Minor fixes to the DateTime implementation

--- indigo-core/src/indigo/core/time/DateTime.scala
+++ indigo-core/src/indigo/core/time/DateTime.scala
@@ -17,10 +17,12 @@ final case class DateTime(
 ) derives CanEqual:
   lazy val (year, month, day, hour, minute, second, millisecond, dayOfWeek) = calculateDateParts
 
-  lazy val epochDay: Long     = Math.floorDiv(localMillisSinceUnixEpoch, 86_400_000L)
-  lazy val epochMillis: Long  = localMillisSinceUnixEpoch - secondsEastOfUtc.toLong * 1000L
-  lazy val epochSeconds: Long = Math.floorDiv(epochMillis, 1000L)
+  lazy private val epochMillis: Long = localMillisSinceUnixEpoch - secondsEastOfUtc.toLong * 1000L
 
+  /** Get the date as it would be in UTC
+    *
+    * @return
+    */
   def asUtc: DateTime = DateTime(epochMillis, 0)
 
   /** Uses Howard Hinnants algorithm for calculating dates using milliseconds since an epoch
@@ -29,26 +31,28 @@ final case class DateTime(
     * @return
     */
   private def calculateDateParts: (Int, Int, Int, Int, Int, Int, Int, Int) =
-    val millisPerDay = 86_400_000L
-    val days         = Math.floorDiv(localMillisSinceUnixEpoch, millisPerDay) // days since 1970-01-01
-    val msOfDay      = Math.floorMod(localMillisSinceUnixEpoch, millisPerDay)
-    val secOfDay     = msOfDay / 1000L
+    import DateTime.Constants.*
 
-    val z   = days + 719468                                         // shift epoch to 0000-03-01
-    val era = (if z >= 0 then z else z - 146096) / 146097           // which 400-year block
-    val doe = z - era * 146097                                      // day of era   [0, 146096]
-    val yoe = (doe - doe / 1460 + doe / 36524 - doe / 146096) / 365 // year of era [0, 399]
+    val days     = Math.floorDiv(localMillisSinceUnixEpoch, millisPerDay) // Days since 1970-01-01
+    val msOfDay  = Math.floorMod(localMillisSinceUnixEpoch, millisPerDay) // The millisecond part of the day
+    val secOfDay = msOfDay / 1000L                                        // The seconds part of the day
+
+    val z   = days + 719468                               // Shift epoch to 0000-03-01 (3 months ahead of 0000-01-01)
+    val era = (if z >= 0 then z else z - 146096) / 146097 // Which 400-year block
+    val doe = z - era * 146097                            // Day of era   [0, 146096]
+    val yoe = (doe - doe / 1460 + doe / 36524 - doe / 146096) / 365 // Year of era [0, 399]
     val y   = yoe + era * 400
-    val doy = doe - (365 * yoe + yoe / 4 - yoe / 100)               // day of year from Mar 1
-    val mp  = (5 * doy + 2) / 153                                   // month, Mar = 0 .. Feb = 11
+    val doy = doe - (365 * yoe + yoe / 4 - yoe / 100)               // Day of year from Mar 1
+    val mp  = (5 * doy + 2) / 153                                   // Month, Mar = 0 .. Feb = 11
 
-    val day         = (doy - (153 * mp + 2) / 5 + 1).toInt // day of month [1, 31]
-    val month       = (if mp < 10 then mp + 3 else mp - 9).toInt // remap to 1..12
-    val year        = (if month <= 2 then y + 1 else y).toInt // Jan/Feb belong to next year
-    val hour        = (secOfDay / 3600).toInt
-    val minute      = ((secOfDay % 3600) / 60).toInt
-    val second      = (secOfDay  % 60).toInt
-    val millisecond = (msOfDay   % 1000).toInt
+    val day   = (doy - (153 * mp + 2) / 5 + 1).toInt       // Day of month [1, 31]
+    val month = (if mp < 10 then mp + 3 else mp - 9).toInt // Remap to January start 1..12
+    val year  = (if month <= 2 then y + 1 else y).toInt    // Jan/Feb belong to next year (due oto previous March start)
+
+    val hour        = (secOfDay / 3600).toInt // Calculate the hour of the day
+    val minute      = ((secOfDay % 3600) / 60).toInt // Calculate the minute of the day
+    val second      = (secOfDay  % 60).toInt         // Calculate the second of the day
+    val millisecond = (msOfDay   % 1000).toInt       // Calculate the millisecond of the day
 
     val dayOfWeek = Math.floorMod(days + 4, 7).toInt // 0 = Sunday, 6 = Saturday
 
@@ -56,3 +60,6 @@ final case class DateTime(
 
 object DateTime:
   given Ordering[DateTime] = Ordering.by(_.epochMillis)
+
+  object Constants:
+    val millisPerDay: Long = 86_400_000L
